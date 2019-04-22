21616ff7af8c1fec3ac189902fa1ef980de6e962
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-04-22 12:40:53 -0400

    bug fix: modified target_hr in if-else switch to string from integer

--- einstein/utils/summary_table.py
+++ einstein/utils/summary_table.py
@@ -1,4 +1,4 @@
-def draw(year, target_hr, grid, model, dictionary):
+def draw(year, target_hr, grid, model_name, dictionary):
     '''Draws a summary table representing the regression statistics
 
     Arguments:
@@ -9,7 +9,7 @@ def draw(year, target_hr, grid, model, dictionary):
         grid (str):
             Grid option selected by user, which represents the grid around
             ATHENS region
-        model (str):
+        model_name (str):
             Model that the data has been trained on
         dictionary (dict):
             Dictionary of metric names and values
@@ -19,12 +19,12 @@ def draw(year, target_hr, grid, model, dictionary):
     print("-" * 75)
     print(f'{" " * 15}NAM-NMM {year} Data - ({grid}, {grid}) Grid Size')
 
-    if target_hr == 1:
+    if target_hr == '1':
         print(f'Target Hour Offset {" " * 12} : {" " * 20} {target_hr} hr')
     else:
         print(f'Target Hour Offset {" " * 12} : {" " * 20} {target_hr} hrs')
 
-    print(f'Model {" " * 25} : {" " * 10} {model}')
+    print(f'Model {" " * 25} : {" " * 10} {model_name}')
 
     for key, value in dictionary.items():
         key_length = len(str(key))
