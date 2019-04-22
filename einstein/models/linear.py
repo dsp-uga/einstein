42c6404c6e302849d806ceb0855458389b940408
Anirudh-Kakarlapudi <mauryakak@gmail.com>
2019-04-22 03:52:16 -0400

    Made code consistent with base

--- einstein/models/linear.py
+++ einstein/models/linear.py
@@ -2,87 +2,88 @@
 A script to implement regression models
 """
 from pyspark.ml.regression import LinearRegression
-from pyspark.ml import Pipeline
-from pyspark.ml.feature import Normalizer
-from pyspark.sql.session import SparkSession
-from pyspark import SparkConf, SparkContext
 from base import Model
-from pyspark.ml.feature import VectorAssembler, StandardScaler
-from pyspark.ml.evaluation import RegressionEvaluator
 
 
-class MultipleRegression(model):
+class MultipleRegression(Model):
+    """A class for multiple linear regression extending an abstract class model.
     """
-    A class for multiple linear regression extending an abstract class model.
-    """
-    def get_parameters(self):
+    def __init__(self, input_cols, **kwargs):
+        """Initialises the class.
 
+        Args:
+            input_cols(list):
+                A list of all the input column names
+            **kwargs:
+                keyword arguments of user defined parameters
         """
-        A method that defines a dictionary of parameters for regression model
-        
+        self.input_cols = input_col
+        self.kwargs = kwargs
+        self.metrics = ["r2", "mae", "rmse"]
+
+    def get_parameters(self, **user_params):
+        """A method that defines a dictionary of parameters for regression model
+
+        Args:
+          **user_params:
+            key word arguments of user defined parameters
         Returns:
             A  dictionary containing all the parameters
         """
-        return {'maxIter': 20, 'regParam': 0.5, 'elasticNetParam': 0.5, 'tol': 1e-06, 'loss': 'squaredError',
-                'epsilon': 1.35}
+        parameter_dict = {'maxIter': 20, 'regParam': 0.5,
+                          'elasticNetParam': 0.5, 'tol': 1e-06,
+                          'loss': 'squaredError', 'epsilon': 1.35}
+        parameter_dict.update(**user_params)
+        return parameter_dict
 
     def model_define(self):
-        """
-        A method which defines the linear regression model
-        
+        """A method which defines the linear regression model
+
         Returns:
-            A linear regression model 
+            A linear regression model
         """
-        params = self.get_parameters()
+        params = self.get_parameters(**self.kwargs)
         lr = LinearRegression(**params)
         return lr
 
-    def flow(self):
-        """
-        A method that defines a pipeline
-        
-        Returns:
-            A pipeline that the model needs to follow
-        """
-        assembler = VectorAssembler(inputCols=["cylinders",
-                                               "displacement",
-                                               "horsepower",
-                                               "weight",
-                                               "acceleration",
-                                               "model year",
-                                               "origin"],
-                                    outputCol="features")
-        scaler = StandardScaler(inputCol="features",
-                                outputCol="scaledFeatures",
-                                withStd=True, withMean=True)
-        model = self.model_define()
-        pipeline = Pipeline(stages=[assembler, scaler, model])
-        return pipeline
-
 
 class RidgeRegression(MultipleRegression):
+    """A class that inherits from MultipleRegression class and implements
+    Ridge regression
     """
-    A class that inherits from MultipleRegression class and implements Ridge regression
-    """
-    def get_parameters(self):
-        """A method that defines a dictionary of parameters for ridge regression model by using L2 norm
+    def get_parameters(self, **user_params):
+        """A method that defines a dictionary of parameters for ridge
+        regression model by using L2 norm
 
-            Returns:
+        Args:
+          **user_params:
+            key word arguments of user defined parameters
+        Returns:
             A  dictionary containing all the parameters
         """
-        return {'maxIter': 20, 'regParam': 0.5, 'elasticNetParam': 0.0, 'tol': 1e-06, 'loss': 'squaredError',
-                'epsilon': 1.35}
+        parameter_dict = {'maxIter': 20, 'regParam': 0.5,
+                          'elasticNetParam': 0.0, 'tol': 1e-06,
+                          'loss': 'squaredError', 'epsilon': 1.35}
+        parameter_dict.update(**user_params)
+        return parameter_dict
 
 
 class LassoRegression(MultipleRegression):
+    """A class that inherits from MultipleRegression class and implements
+    Lasso regression
     """
-    A class that inherits from MultipleRegression class and implements Lasso regression
-    """
-    def get_parameters(self):
-        """A method that defines a dictionary of parameters for lasso regression model by using L1 norm
+    def get_parameters(self, **user_params):
+        """A method that defines a dictionary of parameters for lasso
+        regression model by using L1 norm
 
-           Returns:
-           A  dictionary containing all the parameters
+        Args:
+          **user_params:
+            key word arguments of user defined parameters
+        Returns:
+            A  dictionary containing all the parameters
         """
-        return {'maxIter': 20, 'regParam': 0.5, 'elasticNetParam': 1.0, 'tol': 1e-06, 'loss': 'squaredError',
-                'epsilon': 1.35}
+        parameter_dict = {'maxIter': 20, 'regParam': 0.5,
+                          'elasticNetParam': 1.0, 'tol': 1e-06,
+                          'loss': 'squaredError', 'epsilon': 1.35}
+        parameter_dict.update(**user_params)
+        return parameter_dict
