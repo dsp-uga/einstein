56509ab58861e9908d70aff6b02aa00fcf8a20aa
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-04-24 18:12:01 -0400

    Solved indentation errors

--- tests/Integration Test/main_test.py
+++ tests/Integration Test/main_test.py
@@ -3,11 +3,11 @@ from einstein.__main__ import run, get_parser
 
 
 def test_get_parser():
-	parser = get_parser()
+    parser = get_parser()
     # Testing if `parser` is an ArgumentParser
     assert isinstance(parameter_dict, ArgumentParser()) == True
 
 def test_run():
-	output = run()
-	# Testing that the model summary table is being printed
-	assert len(str(output)) > 0
+    output = run()
+    # Testing that the model summary table is being printed
+    assert len(str(output)) > 0
