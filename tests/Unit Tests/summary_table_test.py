2b1846b17808bafac29f127e5248397ce243c719
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-04-24 17:58:51 -0400

    Added unit test for utils/summary_table.py

---
+++ tests/Unit Tests/summary_table_test.py
@@ -0,0 +1,13 @@
+from einstein.utils.summary_table import draw
+
+
+def test_draw():
+	year = '2017'
+	target_hr = '1'
+	grid = '1'
+	model_name = 'abc'
+	dictionary = { 'a': 1, 'b': 2}
+	output = draw(year, target_hr, grid, model_name, dictionary)
+	# Testing that there is atleast something which is being printed
+	assert len(str(output)) > 0
+
