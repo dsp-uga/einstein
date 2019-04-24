b1dcfb328194012be3cbdcaa7bde61c5fe5eb5d1
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-04-24 17:59:20 -0400

    Added integration test for project

---
+++ tests/Integration Test/main_test.py
@@ -0,0 +1,13 @@
+import argparse
+from einstein.__main__ import run, get_parser
+
+
+def test_get_parser():
+	parser = get_parser()
+    # Testing if `parser` is an ArgumentParser
+    assert isinstance(parameter_dict, ArgumentParser()) == True
+
+def test_run():
+	output = run()
+	# Testing that the model summary table is being printed
+	assert len(str(output)) > 0
