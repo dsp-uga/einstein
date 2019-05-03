f8912c624d62685bc1eca9904bb8783bc958e846
Anirudh-Kakarlapudi <mauryakak@gmail.com>
2019-05-03 18:51:52 -0400

    Removed an extra argument for read_csv fucntion

--- einstein/reader.py
+++ einstein/reader.py
@@ -13,27 +13,24 @@ findspark.init()
 from pyspark.sql import SparkSession
 
 
-def read_csv(file_name, key_path='gs://dsp_uga/',
-             encrypt_path='gs://profinal/',
+def read_csv(file_name, encrypt_path='gs://profinal/',
              decrypt_path='gs://dsp_uga/'):
     """Reads the spark dataframe
 
     Args:
         file_name(str):
             Name of the csv file to be read as spark dataframe
-        key_path(str):
-            A google storage path where the key is stored
         decrypt_path(str):
             A google storage path where the decrypt files should be stored
         encrypt_path(str):
             A google storage path where the enrypted files are stored
-	Returns:
-		data_frame:(data frame)
+    Returns:
+        data_frame:(data frame)
     """
     spark = SparkSession.builder.master("yarn").appName(
         "Solar Irradiance Prediction").getOrCreate()
-    dec = Decryption(key_path, encrypt_path, decrypt_path)
+    dec = Decryption(encrypt_path, decrypt_path)
     dec.decrypt(file_name)
-    data_frame = spark.read.csv(os.path.join(decrypt_path, file_name), header='true', inferSchema='true')
-    #dec.delete_data(file_name, decrypt_path)
+    data_frame = spark.read.csv(os.path.join(decrypt_path, file_name),
+                                header='true', inferSchema='true')
     return data_frame
