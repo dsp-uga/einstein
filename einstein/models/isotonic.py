a3b47462ef605ab1dff29da36b5d16f41fcb8882
jayant1234 <parasharjjjayant@gmail.com>
2019-05-02 20:40:23 -0400

    implemented cross val for linear and isotonic regressions

---
+++ einstein/models/isotonic.py
@@ -0,0 +1,74 @@
+"""
+This script implements the :class: `IsotonicRegressor`, sub-classed on :class:
+`Model`, which implements the Isotonic Regression model.
+
+Author:
+----------
+Jayant Parashar
+"""
+import findspark
+findspark.init()
+
+from pyspark.ml.regression import IsotonicRegression
+from einstein.models.base import Model
+from pyspark.ml.tuning import ParamGridBuilder
+
+class IsotonicRegressor(Model):
+    """Subclasses base :class: `Model` to initialize an Isotonic Regression
+    """
+    def __init__(self, input_cols, **kwargs):
+        """Initialises the IsotonicRegressor class.
+
+        Args:
+            input_cols (list):
+                List of all the input column names
+            **kwargs (dict):
+                Dictionary of keyword arguments for user defined parameters
+        """
+        self.input_cols = input_cols
+        self.kwargs = {k: v for k, v in kwargs.items() if v is not None}
+        self.metrics = ["r2", "mae", "rmse"]
+
+    def get_parameters(self, user_params):
+        """Declares a dictionary of hyperparameters for Regression.
+
+        Args:
+            user_params (dict):
+                Dictionary of keyword arguments for user defined parameters
+        Returns:
+            (dict):
+                Dictionary containing all the parameters
+        """
+        parameter_dict = {"featuresCol": "scaledFeatures",
+                          "isotonic": True}
+        parameter_dict.update(user_params)
+        for k, v in parameter_dict.items():
+            print(f'{k} : {v}')
+        return parameter_dict
+
+    def model_define(self):
+        """Returns a model with the hyperparameters inputted in :func:
+        `get_parameters`
+
+        Returns:
+            (pyspark.ml.regression.DecisionTreeRegressor):
+                Decision Tree Regression model
+        """
+        return IsotonicRegression()
+
+    def get_param_grid(self, model):
+        """Defines a parameter grid for crossvalidation with the user-defined
+        model parameters
+    
+        Args:
+            model (pyspark.ml.regression):
+                Regression model for which ParamGrid is to be built
+        Returns:
+            param_grid (ParamGrid):
+                Grid of tunable hyperparameters
+        """
+        params = self.get_parameters(self.kwargs)
+        param_grid = ParamGridBuilder()\
+            .addGrid(model.isotonic, [params['isotonic']])\
+            .build()
+        return param_grid
--- einstein/models/linear.py
+++ einstein/models/linear.py
@@ -10,130 +10,154 @@ Jayant Parashar
 import findspark
 findspark.init()
 
+from pyspark.ml.tuning import ParamGridBuilder
 from pyspark.ml.regression import LinearRegression
 from einstein.models.base import Model
-from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
-
 
 
 class LinearRegressor(Model):
     """A class for multiple linear regression extending an abstract class model.
     """
     def __init__(self, input_cols, **kwargs):
-        """Initialises the class.
+        """Initialises the LinearRegressor class.
 
         Args:
-            input_cols(list):
-                A list of all the input column names
-            **kwargs:
-                keyword arguments of user defined parameters
+            input_cols (list):
+                List of all the input column names
+            **kwargs (dict):
+                Dictionary of keyword arguments for user defined parameters
         """
         self.input_cols = input_cols
         self.kwargs = {k: v for k, v in kwargs.items() if v is not None}
         self.metrics = ["r2", "mae", "rmse"]
 
-    def get_parameters(self, **user_params):
-        """A method that defines a dictionary of parameters for regression model
+    def get_parameters(self, user_params):
+        """Declares a dictionary of hyperparameters for Regression.
 
         Args:
-          **user_params:
-            key word arguments of user defined parameters
+            user_params (dict):
+                Dictionary of keyword arguments for user defined parameters
         Returns:
-            A  dictionary containing all the parameters
+            (dict):
+                Dictionary containing all the parameters
         """
         parameter_dict = {'maxIter': 20, 'regParam': 0.5,
                           'elasticNetParam': 0.5, 'tol': 1e-06,
                           'loss': 'squaredError', 'epsilon': 1.35}
-        parameter_dict.update(**user_params)
+        parameter_dict.update(user_params)
         for k, v in parameter_dict.items():
             print(f'{k} : {v}')
         return parameter_dict
 
     def model_define(self):
-        """A method which defines the linear regression model
+        """Returns a model with the hyperparameters inputted in :func:
+        `get_parameters`
 
         Returns:
-            A linear regression model
+            (pyspark.ml.regression.LinearRegression):
+                Linear Regression model
         """
-        params = self.get_parameters(**self.kwargs)
-        linear_reg = LinearRegression(**params)
-        return linear_reg
+        return LinearRegression()
 
-    def fit_transform(self, train_data, test_data):
-        """A method to train the model and do predictions.
-        a function for calculating the metric value is called
-
+    def get_param_grid(self, model):
+        """Defines a parameter grid for crossvalidation with the user-defined
+        model parameters
+    
         Args:
-            train_data: the data on which the model should be trained
-            test_data: the data on which the predictions are to be made
-        Returns:
-            A list containing metric values ["r2", "mae", "rmse"]
+            model (pyspark.ml.regression):
+                Regression model
         """
-        pipeline = self.flow()
-        evaluator = RegressionEvaluator(labelCol='label',
-                                        predictionCol='prediction',
-                                        metricName="rmse")
-        paramGrid = ParamGridBuilder() \
-            .addGrid(lr.regParam, [0.5]) \
+        params = self.get_parameters(self.kwargs)
+        param_grid = ParamGridBuilder()\
+            .addGrid(model.maxIter, [params['maxIter']])\
+            .addGrid(model.regParam, [params['regParam']])\
+            .addGrid(model.elasticNetParam, [params['elasticNetParam']])\
+            .addGrid(model.tol, [params['tol']])\
+            .addGrid(model.loss, [params['loss']])\
+            .addGrid(model.epsilon, [params['epsilon']])\
             .build()
-        crossval = CrossValidator(estimator=pipeline,
-                                  estimatorParamMaps=paramGrid,
-                                  evaluator=evaluator,
-                                  numFolds=4)
-        cvmodel = crossval.fit(training)
-        predictions = cvmodel.transform(test_data)
-        predictions = predictions['prediction', 'label']
-        cv_metric_rmse = evaluator.evaluate(predictions)
-
-        model = pipeline.fit(train_data)
-        predictions = model.transform(test_data)
-        predictions = predictions['prediction', 'label']
-        metric_list=self.get_accuracy(predictions)
-        metric_list.append(cv_metric_rmse)
-        return metric_list
+        return param_grid
 
 
 class RidgeRegressor(LinearRegressor):
     """A class that inherits from LinearRegressor class and implements
     Ridge regression
     """
-    def get_parameters(self, **user_params):
-        """A method that defines a dictionary of parameters for ridge
-        regression model by using L2 norm
+    def get_parameters(self, user_params):
+        """Defines a Ridge Regression model using the L2 Norm, on a dictionary
+        of parameters
 
         Args:
-          **user_params:
-            key word arguments of user defined parameters
+            user_params (dict):
+                Dictionary of keyword arguments for user defined parameters
         Returns:
-            A  dictionary containing all the parameters
+            (dict):
+                Dictionary containing all the parameters
         """
         parameter_dict = {'maxIter': 20, 'regParam': 0.5,
                           'elasticNetParam': 0.0, 'tol': 1e-06,
                           'loss': 'squaredError', 'epsilon': 1.35}
-        parameter_dict.update(**user_params)
+        parameter_dict.update(user_params)
         for k, v in parameter_dict.items():
             print(f'{k} : {v}')
         return parameter_dict
+
+    def get_param_grid(self, model):
+        """Defines a parameter grid for crossvalidation with the user-defined
+        model parameters
+    
+        Args:
+            model (pyspark.ml.regression):
+                Regression model
+        """
+        params = self.get_parameters(self.kwargs)
+        param_grid = ParamGridBuilder()\
+            .addGrid(model.maxIter, [params['maxIter']])\
+            .addGrid(model.regParam, [params['regParam']])\
+            .addGrid(model.elasticNetParam, [params['elasticNetParam']])\
+            .addGrid(model.tol, [params['tol']])\
+            .addGrid(model.loss, [params['loss']])\
+            .build()
+        return param_grid
 
 
 class LassoRegressor(LinearRegressor):
     """A class that inherits from LinearRegressor class and implements
     Lasso regression
     """
-    def get_parameters(self, **user_params):
-        """A method that defines a dictionary of parameters for lasso
-        regression model by using L1 norm
+    def get_parameters(self, user_params):
+        """Defines a Lasso Regression model using the L1 Norm, on a dictionary
+        of parameters
 
         Args:
-          **user_params:
-            key word arguments of user defined parameters
+            user_params (dict):
+                Dictionary of keyword arguments for user defined parameters
         Returns:
-            A  dictionary containing all the parameters
+            (dict):
+                Dictionary containing all the parameters
         """
         parameter_dict = {'maxIter': 20, 'regParam': 0.5,
                           'elasticNetParam': 1.0, 'tol': 1e-06,
                           'loss': 'squaredError', 'epsilon': 1.35}
-        parameter_dict.update(**user_params)
+        parameter_dict.update(user_params)
         for k, v in parameter_dict.items():
             print(f'{k} : {v}')
         return parameter_dict
+
+    def get_param_grid(self, model):
+        """Defines a parameter grid for crossvalidation with the user-defined
+        model parameters
+    
+        Args:
+            model (pyspark.ml.regression):
+                Regression model
+        """
+        params = self.get_parameters(self.kwargs)
+        param_grid = ParamGridBuilder()\
+            .addGrid(model.maxIter, [params['maxIter']])\
+            .addGrid(model.regParam, [params['regParam']])\
+            .addGrid(model.elasticNetParam, [params['elasticNetParam']])\
+            .addGrid(model.tol, [params['tol']])\
+            .addGrid(model.loss, [params['loss']])\
+            .build()
+        return param_grid
