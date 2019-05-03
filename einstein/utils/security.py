fbcaa7089fa1546d4fe5abcd6bac0625a5258247
Anirudh-Kakarlapudi <mauryakak@gmail.com>
2019-05-02 20:30:16 -0400

    Added a method to delete data after reading

--- einstein/utils/security.py
+++ einstein/utils/security.py
@@ -1,16 +1,16 @@
 """
-This script implements the :class: Encryption, and :class: Decryption
+This script implements the :class: `Encryption`, and :class: `Decryption`
 which encrypt and decrypt the data files.
 
 Author:
 ----------
 Anirudh K.M. Kakarlapudi
 """
-import pyAesCrypt
-import binascii
 import os
-import subprocess
 import shutil
+import binascii
+import subprocess
+import pyAesCrypt
 
 
 class Encryption:
@@ -22,14 +22,14 @@ class Encryption:
         """Initialises the class.
 
         Args:
-            key_path(path):
+            key_path(str):
                 A google storage path where the key should be stored
-            file_path(path):
+            file_path(str):
                 A google storage path where the data files are stored
-            encrypt_path(path):
+            encrypt_path(str):
                 A google storage path where the enrypted files should be stored
         """
-        self.bufferSize = 64 * 1024
+        self.buffer_size = 64 * 1024
         self.key_path = key_path
         self.encrypt_path = encrypt_path
         self.file_path = file_path
@@ -39,8 +39,8 @@ class Encryption:
         storage bucket
 
         Args:
-            key_file_name:
-                    The name of the key file
+            key_file_name(str):
+                The name of the key file
         Returns:
             The 16 digit key
         """
@@ -63,38 +63,46 @@ class Encryption:
                   if file.endswith('.csv')]
         for file in files:
             pyAesCrypt.encryptFile('temp/' + file, 'temp/' + file + ".aes",
-                                   self.key, self.bufferSize)
+                                   self.key, self.buffer_size)
             self.upload(file + ".aes", self.encrypt_path)
         shutil.rmtree('temp')
 
     def download(self, gspath):
         """Function to download all the files in the storage bucket
+
+        Args:
+            gspath(str):
+                 A google storage path where the enrypted files should be
+                 stored
         """
         subprocess.run(f'gsutil rsync -r {gspath} ~/temp/', shell=True)
 
-    def upload(self, filename, gspath):
+    def upload(self, file_name, gspath):
         """Function to upload the file into the storage bucket
+        Args:
+            file_name(str):
+                The name of file to be uploaded
         """
-        subprocess.run(f'gsutil -m cp ~/temp/{filename} {gspath}', shell=True)
+        subprocess.run(f'gsutil -m cp ~/temp/{file_name} {gspath}', shell=True)
 
 
 class Decryption:
     """A class for decryting the data files
     """
-    def __init__(self, key_path='gs://dsp_uga/',
-                 decrypt_path='gs://dsp_uga/',
-                 encrypt_path='gs://profinal/'):
+    def __init__(self, key_path,
+                 encrypt_path,
+                 decrypt_path):
         """Initialises the class.
 
         Args:
-            key_path(path):
+            key_path(str):
                 A google storage path where the key should be stored
-            decrypt_path(path):
+            decrypt_path(str):
                 A google storage path where the decrypt files should be stored
-            encrypt_path(path):
+            encrypt_path(str):
                 A google storage path where the enrypted files are stored
         """
-        self.bufferSize = 64 * 1024
+        self.buffer_size = 64 * 1024
         self.key_path = key_path
         self.encrypt_path = encrypt_path
         self.decrypt_path = decrypt_path
@@ -104,8 +112,8 @@ class Decryption:
         storage bucket
 
         Args:
-            key_file_name:
-                    The name of the key file
+            key_file_name(str):
+                The name of the key file
         Returns:
             The 16 digit key
         """
@@ -113,51 +121,56 @@ class Decryption:
         with open('temp/'+key_file_name, 'r') as file:
             self.key = file.read()
 
-    def decrypt(self, filename):
+    def decrypt(self, file_name):
         """Function to encrypt all the files in the bucket.
         Uploads all the files into the bucket.
-        """
-        os.mkdir('temp')
-        self.download(filename + ".aes", self.encrypt_path)
-        self.load_key('key.txt')
-        pyAesCrypt.decryptFile('temp/' + filename + ".aes", 'temp/' + filename,
-                               self.key, self.bufferSize)
-        self.upload(filename, self.decrypt_path)
-        shutil.rmtree('temp')
-
-    def delete_data(self, filename, gspath):
-        """Function to delete all the files in the temp folder
 
         Args:
-            filename:
+            file_name(str):
+                The name of file to be decrypted
+        """
+        os.mkdir('temp')
+        self.download(file_name + ".aes", self.encrypt_path)
+        self.load_key('key.txt')
+        pyAesCrypt.decryptFile('temp/' + file_name + ".aes", 'temp/' + file_name,
+                               self.key, self.buffer_size)
+        self.upload(file_name, self.decrypt_path)
+        shutil.rmtree('temp')
+
+    def delete_data(self, file_name, gspath):
+        """Deletes all the files in the temp folder
+
+        Args:
+            file_name(str):
                 The name of the file to be removed from google storage bucket
-            gspath:
+            gspath(str):
                 The google storage bucket path from where the file should be
                 deleted
         """
-        subprocess.run(f'gsutil rm {gspath}{filename}', shell=True)
+        subprocess.run(f'gsutil rm {gspath}{file_name}', shell=True)
 
-    def download(self, filename, gspath):
-        """Function to download the file in the storage bucket
+    def download(self, file_name, gspath):
+        """Downloads the file in the storage bucket
 
         Args:
-            filename:
+            file_name(str):
                 The name of the file to be downloaded from google storage
                 bucket
-            gspath:
+            gspath(str):
                 The google storage bucket path from where the file should be
                 downloaded
         """
-        subprocess.run(f'gsutil -m cp {gspath}{filename} ~/temp/', shell=True)
+        print(f'gsutil -m cp {gspath}{file_name} ~/temp')
+        subprocess.run(f'gsutil -m cp {gspath}{file_name} ~/temp/', shell=True)
 
-    def upload(self, filename, gspath):
-        """Function to upload the file in the storage bucket
+    def upload(self, file_name, gspath):
+        """Uploads the file in the storage bucket
 
         Args:
-            filename:
+            file_name(str):
                 The name of the file to be uploaded from google storage bucket
-            gspath:
+            gspath(str):
                 The google storage bucket path from where the encrypt file
                 should be uploaded
         """
-        subprocess.run(f'gsutil -m cp ~/temp/{filename} {gspath}', shell=True)
+        subprocess.run(f'gsutil -m cp ~/temp/{file_name} {gspath}', shell=True)
